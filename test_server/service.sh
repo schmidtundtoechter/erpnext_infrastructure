9102d02e97827bc7046b2af9da010200568cb493
Marcus Wiegand <marcus.wiegand@phamos.eu>
2025-01-08 14:41:57 +0100

    IP aktualisiert und Ersteingabe Lead erstellt

--- test_server/service.sh
+++ test_server/service.sh
@@ -39,18 +39,18 @@ function local.install_update() {
   mkdir -p ~/.ssh;
   echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa;
   chmod 600 ~/.ssh/id_rsa;
-  if [ ! -d /home/frappe/frappe-bench/apps/test12 ]; then
-      echo "App test12 does not exist - cloning";
-      GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" bench get-app git@github.com:larsmaeurer/test12.git;
+  if [ ! -d /home/frappe/frappe-bench/apps/ersteingabe_lead ]; then
+      echo "App ersteingabe_lead does not exist - cloning";
+      GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" bench get-app git@github.com:larsmaeurer/ersteingabe_lead.git;
   else
-      echo "App test12 already exists - pulling latest changes";
-      cd /home/frappe/frappe-bench/apps/test12;
+      echo "App ersteingabe_lead already exists - pulling latest changes";
+      cd /home/frappe/frappe-bench/apps/ersteingabe_lead;
       GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git pull;
   fi;
   rm -rf ~/.ssh/id_rsa
 
-  echo "Install test12 app and migrate";
-  bench --site $SITE install-app test12;
+  echo "Install ersteingabe_lead app and migrate";
+  bench --site $SITE install-app ersteingabe_lead;
   bench --site $SITE migrate;
 EOL
 
